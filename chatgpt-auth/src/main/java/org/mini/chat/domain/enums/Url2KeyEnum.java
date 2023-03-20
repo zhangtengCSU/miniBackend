106397fc0089f0dbc51d0180b015cd6fdd535ff3
zhangtengCSU <b969853447@qq.com>
2023-03-20 23:35:02 +0800

    random

--- chatgpt-auth/src/main/java/org/mini/chat/domain/enums/Url2KeyEnum.java
+++ chatgpt-auth/src/main/java/org/mini/chat/domain/enums/Url2KeyEnum.java
@@ -6,20 +6,20 @@ import java.util.Random;
 
 @Getter
 public enum Url2KeyEnum {
-    URL1("https://chatgptforwechat.azurewebsites.net/api/chatgpt_for_wechat?code=","stdServerCode"),
-    URL2("https://gpt-test.azurewebsites.net/api/chatgpt_for_wechat?code=","testCode")
-    ;
+    URL1("https://chatgptforwechat.azurewebsites.net/api/chatgpt_for_wechat?code=", "stdServerCode"),
+    URL2("https://gpt-test.azurewebsites.net/api/chatgpt_for_wechat?code=", "testCode");
 
     private String url;
     private String keyName;
 
-    Url2KeyEnum(String url,String keyName) {
+    Url2KeyEnum(String url, String keyName) {
         this.url = url;
         this.keyName = keyName;
     }
 
     public static Url2KeyEnum selectUrl() {
-        if (new Random().nextBoolean()) {
+        int seed = (int) (Math.random() * 100) + 1;
+        if (seed % 2 == 0) {
             return URL1;
         } else {
             return URL2;
